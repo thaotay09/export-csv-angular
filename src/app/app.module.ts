61ddacb0651436d8115472b1c278347be25c6ed4
ThaoTay <maiphuongthao2709@gmail.com>
2019-09-26 15:37:31 +0700

    OCIO export report conversation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserComponent } from './user/user.component'
+import { Routes, RouterModule } from '@angular/router';
+import { UserService } from './user.service';
+import { ExportCsvComponent } from './export-csv/export-csv.component';
+import { AuthguardGuard } from './authguard.guard';
+
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthguardGuard],
+    component: ExportCsvComponent
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    UserComponent,
+    ExportCsvComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    
   ],
-  providers: [],
+  providers: [UserService, AuthguardGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  constructor(private router:Router, private user:UserService) { }
+
+  ngOnInit() {
+    console.log('hit');
+  }
+  loginUser(e) {
+  	e.preventDefault();
+  	console.log(e);
+  	var username = e.target.elements[0].value;
+  	var password = e.target.elements[1].value;
+  	
+  	if(username == 'admin' && password == 'admin') {
+      this.user.setUserLoggedIn();
+  		this.router.navigate(['dashboard']);
+  	}
+  }
+
+}
